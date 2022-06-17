3f08946f23f2efc5d41c4da2655b1426975b7ab5
Jiuk Choi <jiukchoi@algotradingsysteminc.com>
2022-06-16 19:59:12 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4>블로그임</h4>
+      </div>
+      <h4>나의 블로그 리스트</h4>
+    </div>
+  );
+}
+
+export default App;

39b2c977cfae3030a80bc0382793ea6ddf9199d1
Jiuk Choi <jiukchoi@algotradingsysteminc.com>
2022-07-28 17:00:49 -0500

    initiate project

---
+++ portfolio/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './components/Header';
+import MainContainer from './components/MainContainer';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
